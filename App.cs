57ef9b4fa7cc605d0404dc0cb2a14959294a5b19
Jean-Michel Deruty <jmderuty@stormancer.com>
2016-03-08 22:35:45 +0100

    test

--- App.cs
+++ App.cs
@@ -11,7 +11,7 @@ namespace Server
     public class App
     {
         public void Run(IAppBuilder builder)
-        {
+        { 
             builder.AddGameScene();
 
             var userConfig = new Users.UserManagementConfig()
